078126d9428c89c4706b26b1e4d86784d78582ae
pedrohrpc <pedrohrpcorrea@gmail.com>
2022-03-07 12:18:08 -0300

    Aulas 3 e 4 Avançando a POO

--- Avancando_a_POO/modelo.py
+++ Avancando_a_POO/modelo.py
@@ -6,8 +6,8 @@ class Programa:
         self.ano = ano
         self._likes = 0
 
-    def dar_like(self):
-        self._likes += 1
+    def dar_like(self, likes = 1):
+        self._likes += likes
 
     @property
     def nome(self):
@@ -20,6 +20,9 @@ class Programa:
     @property
     def likes(self):
         return self._likes
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.likes} Likes'
     
 
 class Filme(Programa):
@@ -28,9 +31,50 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'{self._nome} - {self.duracao} minutos - {self.ano} - {self.likes} Likes'
+
 class Serie(Programa):
     
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f'{self._nome} - {self.temporadas} temporadas - {self.ano} - {self.likes} Likes'
     
+class Playlist():
+    
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
+vingadores.dar_like(5)
+atlanta.dar_like(3)
+tmep.dar_like()
+demolidor.dar_like(4)
+
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+
+playlist_fim_de_semana = Playlist('Fim de Semana', filmes_e_series)
+
+print(f'Tamanho playlist: {playlist_fim_de_semana.tamanho}')
+
+for programa in playlist_fim_de_semana.listagem:
+    #detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas               #IF-ELSE em uma linha
+    #print(f'{programa.nome} - {detalhes} - {programa.likes}')              #Formatação do print
+    print(programa)
